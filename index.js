933b3d639a376bff9a4f0272bc19c6ef1c262126
0xHy0kR1 <helloweenhell666@gmail.com>
2023-11-09 22:26:28 +0530

    imported some modules and fixing some typos

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // Import required library
+const connectToMongo = require('./db');
 const express = require('express');
+const cors = require('cors'); // Used for handling cross-origin requests
+const morgan = require('morgan'); // Used for logging
 
 connectToMongo(); // Connecting to MongoDB
 
@@ -7,9 +10,19 @@ connectToMongo(); // Connecting to MongoDB
 const app = express();
 const port = 5000;
 
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json())
+
 // Available Routes
-app.use('/api/auth', require('./routes/auth'));  //This response is shown when the user visit "http://localhost:5000/api/auth"
-app.use('/api/notes', require('./routes/notes'));
+app.use('/api/auth', require('./routes/authRoutes'));  //This response is shown when the user visit "http://localhost:5000/api/auth"
+app.use('/api/lost-items', require('./routes/lostItemRoutes')); //This response is shown when the user visit "http://localhost:5000/api/lost-items"
+
+// Error Handling Middleware
+// app.use((error, req, res, next) => {
+//   console.error(error);
+//   res.status(error.status || 500).json({ error: error.message });
+// }); 
 
 // Start the Express server
 app.listen(port, () => {
